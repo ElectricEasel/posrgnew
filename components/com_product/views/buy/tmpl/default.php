aa5e69166d3cbf39fa645da97a286a8b9c84c323
Ricky Goldman <ricky@electriceasel.com>
2014-10-22 11:36:03 -0500

    styling product order form page

--- components/com_product/views/buy/tmpl/default.php
+++ components/com_product/views/buy/tmpl/default.php
@@ -10,16 +10,16 @@ JRequest::setVar('form_name', 'Buy Now: Specials');
 	<div class='gm-head' style="margin-bottom:0;">
 		<h2>Current Specials</h2>
 	</div>
-	<div class='gm-item-form' style="margin-bottom: 15px;">
-		<img class="size-mlarge img-left" src="<?php echo $this->image; ?>" alt="<?php echo $this->item->product_name; ?>" />
+	<div class='gm-item-form'>
+		<img class="pull-left" src="<?php echo $this->image; ?>" alt="<?php echo $this->item->product_name; ?>" />
 		<h4 class="spec-name"><?php echo $this->item->product_name; ?></h4>
 		<p class="product-desc"><?php echo $this->item->product_des; ?></p>
 		<p class="price">Price: <span class="gm-price">$<?php echo  number_format($this->item->price, 2);  ?></span></p>
 		<p class="contact-text">Fill out the form below and one of our representatives will get right back to you. Or call us toll free at 866-462-1005</p>
-		<br class="clear"/>
+		<div class="clear"></div>
 	</div>
-	<span style="margin-bottom:10px;display:block;"><span style="color:red;">*</span> required fields</span>
+	<span class="required-key"><span style="color:red;">*</span> required fields</span>
 	<div class="specials-form-wrap">
-		<?php echo RSFormProHelper::displayForm(3); ?>
+		<?php echo RSFormProHelper::displayForm(1); ?>
 	</div>
 </div>
